8c9a97daa259bcd2c4c980603ba72f975ffa2d00
Najma <najmadwiharani@upi.edu>
2023-06-06 12:39:32 +0700

    pinjaman

--- lib/borrower/pages/pinjaman_borrower.dart
+++ lib/borrower/pages/pinjaman_borrower.dart
@@ -14,26 +14,226 @@ class HistoryPeminjamanPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 5, // Replace with the actual number of history items
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      DetailPeminjamanPage(), // Replace with your detail page widget
+          itemCount: 5, // Replace with the actual number of history items
+          itemBuilder: (context, index) {
+            return _buildProductCard(context, 'Peminjaman 1', 'Investor A');
+          }),
+    );
+  }
+
+  Widget _buildProductCard(
+      BuildContext context, String name, String description) {
+    return Card(
+      elevation: 2.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          description,
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                    CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Color.fromARGB(95, 182, 233, 183),
+                        child: Text("A")),
+                  ],
                 ),
-              );
-            },
-            child: Card(
-              child: ListTile(
-                title: Text(
-                    'History Item $index'), // Replace with your history item details
-              ),
+                SizedBox(height: 8.0),
+              ],
             ),
-          );
-        },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [Text('PLAFOND'), Text('Rp 5.000.000')],
+              ),
+              Column(
+                children: [Text('%BAGI HASIL'), Text('12%')],
+              ),
+              Column(
+                children: [Text('TENOR'), Text('50 Minggu')],
+              ),
+            ],
+          ),
+          SizedBox(height: 16.0),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HistoryPinjamanDetailsScreen(
+                        name: name, description: description),
+                  ),
+                );
+              },
+              child: Text('View Details'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HistoryPinjamanDetailsScreen extends StatelessWidget {
+  final String name;
+  final String description;
+
+  const HistoryPinjamanDetailsScreen({
+    required this.name,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pinjaman Detail'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.topCenter,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      width: double.infinity,
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 211, 195, 195),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Peminjaman 1',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Investor A',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                children: [
+                                  Text('PLAFOND'),
+                                  Text('Rp 5.000.000')
+                                ],
+                              ),
+                              Column(
+                                children: [Text('%BAGI HASIL'), Text('12%')],
+                              ),
+                              Column(
+                                children: [Text('TENOR'), Text('50 Minggu')],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    // CircleAvatar(
+                    //   backgroundImage: AssetImage('assets/logo.png'),
+                    //   radius: 50,
+                    // ),
+                  ],
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Detail Pinjaman',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 5),
+                Divider(
+                  thickness: 3,
+                ),
+                SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Status',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Completed',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Status',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Completed',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Status',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Completed',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
