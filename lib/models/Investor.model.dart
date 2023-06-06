78c38898b32bd4225ccd942a88833d15af51fe28
mfakhrif03 <mfakhrif@upi.edu>
2023-06-06 11:14:22 +0700

    investor_model

--- lib/models/Investor.model.dart
+++ lib/models/Investor.model.dart
@@ -2,65 +2,57 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class User {
-  int idUser;
-  String email;
-  String password;
-  String noTelp;
-  String fotoProfil;
-  int saldo;
-  String tipeUser;
-  int idTipeUser;
+class Investor {
+  int idInvestor;
+  String namaLengkap;
+  String tanggalLahir;
+  String jenisKelamin;
+  String nik;
+  String fotoKtp;
+  String fotoPemilik;
+  int omzet;
 
-  User({
-    required this.idUser,
-    required this.email,
-    required this.password,
-    required this.noTelp,
-    required this.fotoProfil,
-    required this.saldo,
-    required this.tipeUser,
-    required this.idTipeUser,
+  Investor({
+    required this.idInvestor,
+    required this.namaLengkap,
+    required this.tanggalLahir,
+    required this.jenisKelamin,
+    required this.nik,
+    required this.fotoKtp,
+    required this.fotoPemilik,
+    required this.omzet,
   });
 }
 
-class UserCubit extends Cubit<User> {
-  String email = "";
-  String password = "";
-
-  UserCubit()
-      : super(User(
-          idUser: 0,
-          email: "",
-          password: "",
-          noTelp: "",
-          fotoProfil: "",
-          saldo: 0,
-          tipeUser: "",
-          idTipeUser: 0,
-        )) {
-    fetchData(email, password);
-  }
+class UmkmCubit extends Cubit<Investor> {
+  UmkmCubit()
+      : super(Investor(
+          idInvestor: 0,
+          namaLengkap: "",
+          tanggalLahir: "",
+          jenisKelamin: "",
+          nik: "",
+          fotoKtp: "",
+          fotoPemilik: "",
+          omzet: 0,
+        ));
 
   //map dari json ke atribut
   void setFromJson(Map<String, dynamic> json) {
-    emit(User(
-      idUser: json["idUser"],
-      email: json["email"],
-      password: json["password"],
-      noTelp: json["noTelp"],
-      fotoProfil: json["fotoProfil"],
-      saldo: json["saldo"],
-      tipeUser: json["tipeUser"],
-      idTipeUser: json["idTipeUser"],
+    emit(Investor(
+      idInvestor: json["idInvestor"],
+      namaLengkap: json["namaLengkap"],
+      tanggalLahir: json["tanggalLahir"],
+      jenisKelamin: json["jenisKelamin"],
+      nik: json["nik"],
+      fotoKtp: json["fotoKtp"],
+      fotoPemilik: json["fotoPemilik"],
+      omzet: json["omzet"],
     ));
   }
 
-  void fetchData(String email, String password) async {
-    this.email = email;
-    this.password = password;
-    String url =
-        "http://127.0.0.1:8000/login_auth/email=$email&password=$password";
+  void fetchData() async {
+    String url = "";
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       setFromJson(jsonDecode(response.body));
